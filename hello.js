3f7ad8d7ff2f32678c46dd4be4d6df91e951c72a
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-07-21 15:45:56 -0400

    first

---
+++ hello.js
@@ -0,0 +1,17 @@
+
+var msg = 'Hello World';
+console.log(msg);
+
+
+let parser = new Parser();
+
+/*
+need to grab podcasts from RSS 
+    (https://bridgetown.podbean.com/feed.xml)
+COmpare dates
+Download latest
+Upload to S3
+Create Job
+Get JSON
+Save as file to OneDrive *Flow?
+*/
