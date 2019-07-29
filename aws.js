d19d682dfd1687aaba1c8176d02431ebcec6f3b6
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-07-28 20:43:08 -0400

    stuck

--- aws.js
+++ aws.js
@@ -1,13 +1,16 @@
 
 var bucketName = 'transribebucket';
-var keyName = 'hello_world.txt'; //mp3 file name change to
+var keyName = 'hello_world'; //mp3 file name change to
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
-  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'}; //body should be .mp3 file
-  s3.putObject(params, function(err, data) {
+  //var params = {Bucket: bucketName, Key: keyName, Body: FileData}; //body should be .mp3 file
+
+  fs.readFile(file.mp3, function(err,FileData) {
+    s3.putObject({Bucket: bucketName, Key: keyName, Body: FileData}, function(err, data) {
     if (err)
       console.log(err)
     else
       console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+  });
   });
--- hello.js
+++ hello.js
@@ -2,6 +2,12 @@ let Parser = require('rss-parser');
 let parser = new Parser();
 const fs = require('fs');
 var request = require('request');
+var bucketName = 'transribebucketkm';
+var keyName = 'hello_world'; 
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+ // var params = {Bucket: bucketName, Key: keyName, Body: FileData}; 
 
 (async () => {
  
@@ -10,8 +16,13 @@ var request = require('request');
   const DownloadLink=feed.items[0].enclosure.url
   console.log(DownloadLink);
 
-  
   var file = request(DownloadLink).pipe(fs.createWriteStream ('file.mp3'))
+  var upload = fs.createReadStream(file);
 
-
-})();
+    s3.putObject({Bucket: bucketName, Key: title, Body: upload}, function(err, data) {
+    if (err)
+      console.log(err)
+    else
+      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+  });
+  });//final
