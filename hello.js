aa63cd90f094b66d40fe22c992929b87354d4850
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-07-31 22:11:05 -0400

    jared

--- hello.js
+++ hello.js
@@ -3,26 +3,26 @@ let parser = new Parser();
 const fs = require('fs');
 var request = require('request');
 var bucketName = 'transribebucketkm';
-var keyName = 'hello_world'; 
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
- // var params = {Bucket: bucketName, Key: keyName, Body: FileData}; 
+parser.parseURL('https://bridgetown.podbean.com/feed.xml', (err, feed) =>
+  request(feed.items[0].enclosure.url, (err, response) =>
+    uploadFileToS3(response, feed.items[0].title + '.mp3', (data) => transcribeAudio())));
 
-(async () => {
- 
-  let feed = await parser.parseURL('https://bridgetown.podbean.com/feed.xml');
-  const title=feed.items[0].title;
-  const DownloadLink=feed.items[0].enclosure.url
-  console.log(DownloadLink);
-
-  var file = request(DownloadLink).pipe(fs.createWriteStream ('file.mp3'))
-  var upload = fs.createReadStream(file);
-
-    s3.putObject({Bucket: bucketName, Key: title, Body: upload}, function(err, data) {
-    if (err)
+const uploadFileToS3 = (file, fileName, callback) => {
+  s3.upload({ Bucket: bucketName, Key: fileName, Body: file }, (err, data) => {
+    if (err) {
       console.log(err)
-    else
-      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+    } else {
+      console.log("Successfully uploaded data to " + bucketName );
+      callback(data);
+    }
   });
-  });//final
+
+}
+
+const transcribeAudio = () => {
+
+}
+//function transribeS3 ())
