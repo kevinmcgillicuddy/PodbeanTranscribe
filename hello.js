e1c2372b114caeb9537c31f077be137d1e631877
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-08-03 14:18:43 -0400

    fixed

--- hello.js
+++ hello.js
@@ -7,31 +7,14 @@ var AWS = require('aws-sdk');
 var transcribeservice = new AWS.TranscribeService();
 var s3 = new AWS.S3();
 
-
-
-parser.parseURL('https://bridgetown.podbean.com/feed.xml', (err, feed) => {
-  request(feed.items[0].enclosure.url,  (err,response,file) => {
-    uploadFileToS3(file, feed.items[0].title + '.mp3',output)
-      console.log(output);
-  })
+parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
+  request(feed.items[0].enclosure.url, function (err,response,file) {
+    console.log(response.body)
+    s3.upload({ Bucket: bucketName, Key: "testperm2.mp3", Body: file }, function (err, data) {
+            transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: data.Location}, MediaFormat: "mp3", TranscriptionJobName: "testing"}, function (err, data){
+                              //console.log(data);           
+                     });
+            
   });
-    
-const uploadFileToS3 = (file, fileName, callback) => {
-    s3.upload({ Bucket: bucketName, Key: fileName, Body: file }, (err, data) => {
-        if (err) {
-            console.log(err)
-          } else {
-            console.log("Successfully uploaded data to " + bucketName );
-            callback(data);
-          }
   });
-};
-
-/*
-const uploadFileToTranscribe = (a,b,c,d,e,f) => {
-  transcribeservice.startTranscriptionJob({LanguageCode: a, Media:{MediaFileUri:b}, MediaFormat: c, }, (err, data) => {
-        if (err) console.log(err, err.stack); 
-        else     
-        console.log(data);           
-
-  });
+});
