46953ed056cf024e03b531a231340d4f86b20701
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-08-04 18:21:58 -0400

    message

--- transcribe.js
+++ transcribe.js
@@ -1,16 +1,16 @@
 let Parser = require('rss-parser');
-let parser = new Parser();
-const fs = require('fs');
+let fs = require('fs');
 var request = require('request');
 var bucketName = 'transcribebucketkm';
 var AWS = require('aws-sdk');
-var transcribeservice = new AWS.TranscribeService();
+let parser = new Parser();
 var s3 = new AWS.S3();
+var transcribeservice = new AWS.TranscribeService();
+
 
 parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
-  request(feed.items[0].enclosure.url, function (err,response,file) {
-    console.log(response.body)
-    s3.upload({ Bucket: bucketName, Key: "testperm2.mp3", Body: file }, function (err, data) {
+  request(feed.items[0].enclosure.url), function (err, response, file )  {
+        s3.upload({ Bucket: bucketName, Key: "testperm3.mp3", Body: file }, function (err, data) {
             transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: data.Location}, MediaFormat: "mp3", TranscriptionJobName: "testing"}, function (err, data){
                               //console.log(data);           
                      });
@@ -18,3 +18,4 @@ parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) 
   });
   });
 });
+//issue is that when I upload file it is corrupt format
