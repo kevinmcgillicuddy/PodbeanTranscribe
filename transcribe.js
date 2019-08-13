08e5776b891b41e88af789e014bc0a5f7ffd1949
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-08-13 17:41:13 -0400

    update

--- transcribe.js
+++ transcribe.js
@@ -6,16 +6,20 @@ var AWS = require('aws-sdk');
 let parser = new Parser();
 var s3 = new AWS.S3();
 var transcribeservice = new AWS.TranscribeService();
+var audioFile = fs.createWriteStream('audioFile.mp3');
 
-
-parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
-  request(feed.items[0].enclosure.url), function (err, response, file )  {
-        s3.upload({ Bucket: bucketName, Key: "testperm3.mp3", Body: file }, function (err, data) {
-            transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: data.Location}, MediaFormat: "mp3", TranscriptionJobName: "testing"}, function (err, data){
-                              //console.log(data);           
-                     });
-            
-  });
-  });
-});
-//issue is that when I upload file it is corrupt format
+parser.parseURL('https://bridgetown.podbean.com/feed.xml', async function (err, feed) {
+    const res = await request(feed.items[0].enclosure.url).pipe(audioFile)
+      if (res.err) {console.log("error")}
+      else {
+       s3.upload({ Bucket: bucketName, Key: "testperm5.mp3", Body: "audioFile.mp3" }, async function (err, data) {
+          const up = await transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: data.Location}, MediaFormat: "mp3", TranscriptionJobName: "testing"}
+              
+                  );
+                  
+            }
+          );
+  
+      }
+    });
+  
