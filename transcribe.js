7d115f4bf6072b01866a5f09eeacb79bcc692d17
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-08-14 23:05:52 -0400

    progress

--- transcribe.js
+++ transcribe.js
@@ -6,19 +6,27 @@ var AWS = require('aws-sdk');
 let parser = new Parser();
 var s3 = new AWS.S3();
 var transcribeservice = new AWS.TranscribeService();
-var audioFile = fs.createWriteStream('audioFile.mp3');
+//var audioFile = fs.createWriteStream('audioFile.mp3');
 
-parser.parseURL('https://bridgetown.podbean.com/feed.xml', async function (err, feed) {
-    const res = await request(feed.items[0].enclosure.url).pipe(audioFile)
-      if (res.err) {console.log("error")}
-      else {
-       s3.upload({ Bucket: bucketName, Key: "testperm5.mp3", Body: "audioFile.mp3" }, async function (err, data) {
-          const up = await transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: data.Location}, MediaFormat: "mp3", TranscriptionJobName: "testing"}
-              
-                  );
-                  
-            }
-          );
-  
-      }
-    });
+
+parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
+  console.log(err);
+  var requestSettings = {
+    method: 'GET',
+    url: feed.items[0].enclosure.url,
+    encoding: null //request puts body response as string by default
+};
+   request(requestSettings,function (err, response, body){
+     console.log(err);
+      fs.writeFile("./p3.mp3",body,function(err){
+        console.log(err);
+        var someDataStream = fs.createReadStream('p3.mp3'); //maybe knox can make this easier so not writing than reading?
+        s3.upload({ Bucket: "transcribebucketkm", Key: "File.mp3", Body: someDataStream},function(err,data){
+            console.log(err);
+            transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: data.Location}, MediaFormat: "mp3", TranscriptionJobName: "testing"});
+            });
+
+      });
+   });
+  });
+      
