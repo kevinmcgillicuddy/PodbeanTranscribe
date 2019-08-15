c162219a140b1d1da44b8dc872c98cd915bb717f
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-08-14 20:25:01 -0400

    fix the wrong way

--- transcribe.js
+++ transcribe.js
@@ -1,6 +1,5 @@
 let Parser = require('rss-parser');
 let fs = require('fs');
-const http = require('http');
 var request = require('request');
 var bucketName = 'transcribebucketkm';
 var AWS = require('aws-sdk');
@@ -9,28 +8,11 @@ var s3 = new AWS.S3();
 var transcribeservice = new AWS.TranscribeService();
 var audioFile = fs.createWriteStream('audioFile.mp3');
 
-
-
-var download = parser.parseURL('https://bridgetown.podbean.com/feed.xml',function(err,feed){
-  
-}
-
-   var request = http.get(download,function(response){
-    response.pipe(audioFile);
-    File.on('finish',function(){
-      s3.upload({ Bucket: bucketName, Key: "mp3.mp3", Body: audioFile });
-    });    
-   });
-    
-
-   /*
-    (feed.items[0].enclosure.url),function(err,response,body){
-      fs.writeFile(mp3.mp3, data,function(){
-        s3.upload({ Bucket: bucketName, Key: "mp3.mp3", Body: "mp3.mp3" }, function (err, data) {
-      }
-    
-    
-       
+parser.parseURL('https://bridgetown.podbean.com/feed.xml', async function (err, feed) {
+    const res = await request(feed.items[0].enclosure.url).pipe(audioFile)
+      if (res.err) {console.log("error")}
+      else {
+       s3.upload({ Bucket: bucketName, Key: "testperm5.mp3", Body: "audioFile.mp3" }, async function (err, data) {
           const up = await transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: data.Location}, MediaFormat: "mp3", TranscriptionJobName: "testing"}
               
                   );
@@ -40,4 +22,3 @@ var download = parser.parseURL('https://bridgetown.podbean.com/feed.xml',functio
   
       }
     });
-  */
