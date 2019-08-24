180561d4df805c7bffec2fa6318bbc1240862975
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-08-24 14:00:06 -0400

    removed fs, formatting

--- transcribe.js
+++ transcribe.js
@@ -1,34 +1,28 @@
-
- //dependancies
-  let Parser = require('rss-parser');
-  let fs = require('fs');
-  var request = require('request');
-  var AWS = require('aws-sdk');
-
+var Parser = require('rss-parser');
+var request = require('request');
+var AWS = require('aws-sdk');
 var bucketName = 'transcribebucketkm';
 var bucketNameOut = 'transcribebucketkm-out';
-let parser = new Parser();
+
+var parser = new Parser();
 var s3 = new AWS.S3();
 var transcribeservice = new AWS.TranscribeService();
+
 AWS.config.update({region:'us-east-1'});
 
+exports.handler = async (event, context) => {
+ 
 var datetime = new Date.toISOString().slice(0,10) + ".mp3";
 var mediafileuri= "http://s3.amazonaws.com/transcribebucketkm/" + datetime
 
 parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
-    var requestSettings = {
-    method: 'GET',
-    url: feed.items[0].enclosure.url,
-    encoding: null}; //request puts body response as string by default
-     request(requestSettings,function (err, response, body){
-
+      request({ method: 'GET', url: feed.items[0].enclosure.url, encoding: null},function (err, response, body){ 
             s3.upload({ Bucket: bucketName, Key: datetime, Body: body},function(err,data){
-              console.log(err)
-              transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafileuri}, MediaFormat: "mp3", TranscriptionJobName: datetime, OutputBucketName: bucketNameOut});
+                  transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafileuri}, MediaFormat: "mp3", TranscriptionJobName: datetime, OutputBucketName: bucketNameOut});
             });
 
       });
-   });
+});
        
 
-  
+}
