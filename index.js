5663ecab3ccb4afd82d7c631b818b3a8b7b6815c
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-08-28 22:00:23 -0400

    fixed datetime

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 exports.handler = async (event, context) => {
 
       var Parser = require('rss-parser');
@@ -13,18 +11,14 @@ var s3 = new AWS.S3();
 var transcribeservice = new AWS.TranscribeService();
 
 AWS.config.update({region:'us-east-1'});
-var datetime = new Date
-var datetime = datetime.toISOString().slice(0,10) + ".mp3";
+var datetime = new Date().toISOString().slice(0,10) + ".mp3";
+console.log(datetime)
 var mediafileuri= "http://s3.amazonaws.com/transcribebucketkm/" + datetime
 
 
 parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
-            console.log(err)
-      request({ method: 'GET', url: feed.items[0].enclosure.url, encoding: null},function (err, response, body){ 
-            console.log(err)
-            cosnsole.log(body)
+             request({ method: 'GET', url: feed.items[0].enclosure.url, encoding: null},function (err, response, body){ 
             s3.upload({ Bucket: bucketName, Key: datetime, Body: body},function(err,data){
-                  console.log(data)
                   transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafileuri}, MediaFormat: "mp3", TranscriptionJobName: datetime, OutputBucketName: bucketNameOut});
             });
 
