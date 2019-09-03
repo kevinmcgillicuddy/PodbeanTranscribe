0112f4cad97687607786396f7862a9f83ff6c6f8
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-09-02 20:50:58 -0400

    resolved ordering of aws config

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 exports.handler = async (event, context) => {
-
-      var Parser = require('rss-parser');
+var Parser = require('rss-parser');
 var request = require('request');
 var AWS = require('aws-sdk');
 var bucketName = 'transcribebucketkm';
 var bucketNameOut = 'transcribebucketkm-out';
-
+AWS.config.update({region:'us-east-1'});
 var parser = new Parser();
 var s3 = new AWS.S3();
 var transcribeservice = new AWS.TranscribeService();
-
-AWS.config.update({region:'us-east-1'});
 var datetime = new Date().toISOString().slice(0,10) + ".mp3";
 console.log(datetime)
 var mediafileuri= "http://s3.amazonaws.com/transcribebucketkm/" + datetime
-
-
 parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
              request({ method: 'GET', url: feed.items[0].enclosure.url, encoding: null},function (err, response, body){ 
             s3.upload({ Bucket: bucketName, Key: datetime, Body: body},function(err,data){
