63d80beb41cee0dff15a2c8fb588ed9b74fdbbd0
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-09-03 20:19:52 -0400

    update title

--- transcribe.js
+++ transcribe.js
@@ -11,18 +11,18 @@ let parser = new Parser();
 var s3 = new AWS.S3();
 var transcribeservice = new AWS.TranscribeService();
 
-var datetime = new Date().toISOString().slice(0,10) + ".mp3";
-var mediafileuri= "http://s3.amazonaws.com/transcribebucketkm/" + datetime
-
 parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
     var requestSettings = {
     method: 'GET',
     url: feed.items[0].enclosure.url,
     encoding: null}; //request puts body response as string by default
-     request(requestSettings,function (err, response, body){
-
-            s3.upload({ Bucket: bucketName, Key: feed.items[0].title, Body: body},function(err,data){
-              transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafileuri}, MediaFormat: "mp3", TranscriptionJobName: feed.items[0].title, OutputBucketName: bucketNameOut},function(err){
+    var title = feed.items[0].title.toString().replace(/\s/g, '') + ".mp3"
+    var mediafileuri= "http://s3.amazonaws.com/transcribebucketkm/" + title
+    console.log(title)
+      request(requestSettings,function (err, response, body){
+        console.log(title)
+            s3.upload({ Bucket: bucketName, Key: title, Body: body},function(err,data){
+              transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafileuri}, MediaFormat: "mp3", TranscriptionJobName: title, OutputBucketName: bucketNameOut},function(err){
                 console.log(err)
               });
             });
