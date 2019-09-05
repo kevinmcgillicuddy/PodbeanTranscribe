7beb2addb8e5dac3d7471d3e97b884b148e7827d
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-09-05 19:34:10 -0400

    error handling

--- transcribe.js
+++ transcribe.js
@@ -1,10 +1,10 @@
 
  //dependancies
  
- let Parser = require('rss-parser');
-  var request = require('request');
-  var AWS = require('aws-sdk');
-  AWS.config.update({region:'us-east-1'});
+let Parser = require('rss-parser');
+var request = require('request');
+var AWS = require('aws-sdk');
+AWS.config.update({region:'us-east-1'});
 var bucketName = 'transcribebucketkm';
 var bucketNameOut = 'transcribebucketkm-out';
 let parser = new Parser();
@@ -18,9 +18,9 @@ parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) 
     encoding: null}; //request puts body response as string by default
     var title = feed.items[0].title.toString().replace(/\s/g, '') + ".mp3"
     var mediafileuri= "http://s3.amazonaws.com/transcribebucketkm/" + title
-    console.log(title)
+    console.log(err)
       request(requestSettings,function (err, response, body){
-        console.log(title)
+        console.log(err)
             s3.upload({ Bucket: bucketName, Key: title, Body: body},function(err,data){
               transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafileuri}, MediaFormat: "mp3", TranscriptionJobName: title, OutputBucketName: bucketNameOut},function(err){
                 console.log(err)
