a8f1da30dc6ae9365392d2303482afd75f795247
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-11-25 17:22:24 -0500

    updates

--- transcribe.js
+++ transcribe.js
@@ -17,10 +17,11 @@ class Transcribtion {
     let parser = new Parser();
     let file = await parser.parseURL(this.URL)
     let title = file.items[0].title.toString().replace(/\s/g, '') + ".mp3"
-    return { file: file, title: title };
+    let url = file.items[0].enclosure.url;
+    return { url: url, title: title };
   }
-  async download(file) {
-    var requestSettings = { method: 'GET', url: file, encoding: null };
+  async download(url) {
+    var requestSettings = { method: 'GET', url: url, encoding: null };
     await request(requestSettings)
   }
 
@@ -36,7 +37,36 @@ class Transcribtion {
 
 
 let scribe = new Transcribtion('transcribebucketkm', 'transcribebucketkm-out', 'https://bridgetown.podbean.com/feed.xml')
-scribe.file().then((blah) => {
-  console.log(blah.title)
-})
+scribe.file().then((res) => {
+    return res
+  }).then()
 
+//  //dependancies
+ 
+//  let Parser = require('rss-parser');
+//  var request = require('request');
+//  var AWS = require('aws-sdk');
+//  AWS.config.update({region:'us-east-1'});
+//  var bucketName = 'transcribebucketkm';
+//  var bucketNameOut = 'transcribebucketkm-out';
+//  let parser = new Parser();
+//  var s3 = new AWS.S3();
+//  var transcribeservice = new AWS.TranscribeService();
+ 
+//  parser.parseURL('https://bridgetown.podbean.com/feed.xml', function (err, feed) {
+//      var requestSettings = {
+//      method: 'GET',
+//      url: feed.items[0].enclosure.url,
+//      encoding: null}; //request puts body response as string by default
+//      var title = feed.items[0].title.toString().replace(/\s/g, '') + ".mp3"
+//      var mediafileuri= "http://s3.amazonaws.com/transcribebucketkm/" + title
+//      console.log(err)
+//        request(requestSettings,function (err, response, body){
+//          console.log(err)
+//              s3.upload({ Bucket: bucketName, Key: title, Body: body},function(err,data){
+//                transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafileuri}, MediaFormat: "mp3", TranscriptionJobName: title, OutputBucketName: bucketNameOut},function(err){
+//                  console.log(err)
+//                });
+//              });
+//        });
+//     });
