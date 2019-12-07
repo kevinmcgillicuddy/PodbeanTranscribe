4aae6a158686ee2d6ea363502454d63a4569d365
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-12-06 19:21:14 -0500

    working

--- transcribe.js
+++ transcribe.js
@@ -13,33 +13,38 @@ class Transcription {
     this.bucketOutput = bucketOutput;
     this.URL = URL;
   }
-  async file() {
+  async mp3Info() {
     let parser = new Parser();
     let file = await parser.parseURL(this.URL)
     let title = file.items[0].title.toString().replace(/\s/g, '') + ".mp3"
     let url = file.items[0].enclosure.url;
-    return { url: url, title: title };
+    let mediafile = "http://s3.amazonaws.com/" + bucketName + "/" + url
+    return { url: url, title: title, mediafile: mediafile };
   }
   async download(url) {
     var requestSettings = { method: 'GET', url: url, encoding: null };
     let downloadedFile = await request(requestSettings)
     return downloadedFile
   }
-  async upload(body) {
-    s3.upload({ Bucket: this.bucketName, Key: 'title', Body: body })
-    console.log('uploading')
+  async upload(body,bucketName) {
+    return s3.upload({ Bucket: bucketName, Key: this.title, Body: body }).promise(); 
 }
+  async convert(mediafile,title,bucketOutput){
+    return transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafile}, MediaFormat: "mp3", TranscriptionJobName: title, OutputBucketName: bucketOutput}).promise();
+  }
 }
 let scribe = new Transcription('transcribebucketkm', 'transcribebucketkm-out', 'https://bridgetown.podbean.com/feed.xml')
-scribe.file().then((res) => {
-  console.log('promise1 resolved', res)
-  return res
+
+scribe.mp3Info().then((response) => {
+  return response
 }).then((res) => {
   return scribe.download(res.url)
 }).then((repo)=>{
   scribe.upload(repo)
+}).then(()=>{
+  scribe.convert();
 }).catch((err)=>{
-  console.log(err)
+  console.log("Error",err)
 })
 
 
