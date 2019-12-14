7ce4d2c49764592f2389dee813f08939180c3a7b
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2019-12-13 22:17:18 -0500

    upload

--- transcribe.js
+++ transcribe.js
@@ -18,7 +18,7 @@ class Transcription {
     let file = await parser.parseURL(this.URL)
     let title = file.items[0].title.toString().replace(/\s/g, '') + ".mp3"
     let url = file.items[0].enclosure.url;
-    let mediafile = "http://s3.amazonaws.com/" + bucketName + "/" + url
+    let mediafile = "http://s3.amazonaws.com/" + this.bucketName + "/" + url
     return { url: url, title: title, mediafile: mediafile };
   }
   async download(url) {
@@ -26,12 +26,12 @@ class Transcription {
     let downloadedFile = await request(requestSettings)
     return downloadedFile
   }
-  async upload(body,bucketName) {
-    return s3.upload({ Bucket: bucketName, Key: this.title, Body: body }).promise(); 
+  async upload(body) {
+    let res = await s3.upload({ Bucket: this.bucketName , Key: "title", Body: body })
+    let job = await transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafile}, MediaFormat: "mp3", TranscriptionJobName: title, OutputBucketName: this.bucketOutput})
+    console.log(res,job)
 }
-  async convert(mediafile,title,bucketOutput){
-    return transcribeservice.startTranscriptionJob({LanguageCode: "en-US", Media:{MediaFileUri: mediafile}, MediaFormat: "mp3", TranscriptionJobName: title, OutputBucketName: bucketOutput}).promise();
-  }
+
 }
 let scribe = new Transcription('transcribebucketkm', 'transcribebucketkm-out', 'https://bridgetown.podbean.com/feed.xml')
 
@@ -41,8 +41,6 @@ scribe.mp3Info().then((response) => {
   return scribe.download(res.url)
 }).then((repo)=>{
   scribe.upload(repo)
-}).then(()=>{
-  scribe.convert();
 }).catch((err)=>{
   console.log("Error",err)
 })
