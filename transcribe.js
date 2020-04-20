83f8049132256e2c7965d83c2137731d3f82d0ac
Kevin McGillicuddy <Kevin.McGillicuddy@sight-sound.com>
2020-04-19 20:17:17 -0400

    initial working with GCP

--- transcribe.js
+++ transcribe.js
@@ -1,76 +1,49 @@
-//dependancies
-let Parser = require('rss-parser');
-var request = require('request');
-var AWS = require('aws-sdk');
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
 const fs = require('fs');
-AWS.config.update({ region: 'us-east-1' });
 
-var transcribeservice = new AWS.TranscribeService();
+// Creates a client
+const client = new speech.SpeechClient();
 
-class Transcription {
-  constructor(bucketName, bucketOutput, URL) {
-    this.bucketName = bucketName;
-    this.bucketOutput = bucketOutput;
-    this.URL = URL;
-  }
-  async mp3Info() {
-    let parser = new Parser();
-    let file = await parser.parseURL(this.URL)
-    let title = file.items[0].title.toString().replace(/\s/g, '') + ".mp3"
-    let url = file.items[0].enclosure.url;
-    let mediafile = "http://s3.amazonaws.com/" + this.bucketName + "/" + url
-    return { url: url, title: title, mediafile: mediafile };
-  }
-  // async download(url) {
-  //   var requestSettings = { method: 'GET', url: url,encoding:null };
-  //   let downloadedFile = await request(requestSettings)
-  //   return downloadedFile
-  // }
-  async upload(url) {
-    let fileStream = fs.createWriteStream('node.mp3');
-    var requestSettings = { method: 'GET', url: url,encoding:null };
-    request(requestSettings,()=>{
-      var s3 = new AWS.S3();
-      s3.putObject({ Bucket: this.bucketName, Key: "title", Body: fileStream, ContentType: "application/octet-stream"})
-    
-    }).pipe(fileStream)
-    
-  }
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+//const gcsUri = 'gs://bucketkevin01/node.mp3';
+const gcsUri = 'gs://bucketkevin01/node.mp3';
+const encoding = 'm4a';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
+
+const config = {
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  languageCode: languageCode,
+};
+
+const audio = {
+  uri: gcsUri,
+  // content: fs.readFileSync('node.mp3').toString('base64'),
+};
+
+const request = {
+  config: config,
+  audio: audio,
+};
+
+async function  time () {
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
+const [operation] = await client.longRunningRecognize(request);
+// Get a Promise representation of the final result of the job
+const [response] = await operation.promise();
+const transcription = response.results
+  .map(result => result.alternatives[0].transcript)
+  .join('\n');
+// console.log(`Transcription: ${transcription}`);
+return transcription;
 }
-// let scribe = new Transcription('transcribebucketkm', 'transcribebucketkm-out', 'https://bridgetown.podbean.com/feed.xml')
 
-// scribe.mp3Info().then((response) => {
-//    scribe.upload(response.url)
-// }).catch((err) => {
-//   console.log("Error", err)
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var s3 = new AWS.S3();
-var params = {
-  Bucket: "transcribebucketkm", 
-  Key: "title"
- };
- s3.getObject(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
-   
- });
+time()
+.then(transcription => console.log(transcription))
+.catch()
 
